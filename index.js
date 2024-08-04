e86190bf8f8bcaec2978011e2063299e84ebf6b2
comradekaushik <anandkaushik260@gmail.com>
2024-08-05 01:32:45 +0530

    Added about and login Modal

---
+++ index.js
@@ -0,0 +1,19 @@
+var aboutModal = document.getElementById("about-modal");
+var loginModal = document.getElementById("login-modal");
+var aboutbtn = document.getElementById("top-header-about-btn");
+
+var closeAboutModalBtn = document.getElementById("about-close-button");
+
+function showmodal(){
+    aboutModal.style.display = "block";
+}
+function closemodal(){
+    aboutModal.style.display = "none";
+}
+function showLoginmodal(){
+    loginModal.style.display = "block";
+}
+function closeLoginmodal(){
+    loginModal.style.display = "none";
+}
+
